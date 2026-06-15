e7464854db893bcb1fdb1ebbf8f48c6971ca638f
majd mahmoud <majd.mahmoud161@gmail.com>
2026-06-15 13:27:41 +0300

    first divided

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './styles/App.css';
-
-
+import React from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+import { useState,UseEffect } from 'react';
 
 function App() {
-  
+ 
   return (
-   <div>
-   
-    </div>
-  )
+    <BrowserRouter>
+      
+    </BrowserRouter>
+  );
 }
 
 export default App
