7090295c11cbd7fabe09fcb26cc0723bbe7b4177
majd mahmoud <majd.mahmoud161@gmail.com>
2026-06-17 01:39:56 +0300

    Reset Pasword done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './features/Auth/pages/SignIn';
-
+import ForgetPassword from './features/Auth/pages/ForgetPassword';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/SignIn' element={<SignIn />} />
+        <Route path='/ForgetPassword' element={<ForgetPassword />} />
+
       </Routes>
     </BrowserRouter>
   );
