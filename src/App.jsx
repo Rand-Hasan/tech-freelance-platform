e3213293644feb1594de080e70f8cde6b99c0545
nermin albsharah <nerminbshara@gmail.com>
2026-06-17 15:29:43 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './features/Auth/pages/SignIn';
-
+import CreateAccount from './features/Auth/pages/CreatAccount';
+import Otp from './features/Auth/pages/Otp';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path='/' element={<CreateAccount />} />
         <Route path='/SignIn' element={<SignIn />} />
+           <Route path='/Otp' element={<Otp />} />
+        <Route path='/CreateAccount' element={<CreateAccount />} />
+        
+        
       </Routes>
     </BrowserRouter>
   );
