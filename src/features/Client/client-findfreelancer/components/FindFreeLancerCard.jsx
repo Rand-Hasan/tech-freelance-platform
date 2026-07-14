49de9f7ea17f71d5ce75a1061489460a9a7ada37
majd mahmoud <majd.mahmoud161@gmail.com>
2026-07-14 23:26:20 +0300

    update

--- src/features/Client/client-findfreelancer/components/FindFreeLancerCard.jsx
+++ src/features/Client/client-findfreelancer/components/FindFreeLancerCard.jsx
@@ -1,41 +1,42 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import "../styles/FindFreeLancerCard.css";
 import { useNavigate } from "react-router-dom";
 
 export default function FindFreeLancerCard() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <Card  className='FindFreeLancerCard' onClick={()=>{
-      navigate("/FreeLanceInfo");
-    }}>
-      <CardContent className='CardContent'>
-       <div className='LeftSide'>
-        <div className='NameAndFirstLater'>
-            <div className='FirstLaterOfFreeLancer'>MA</div>
-            <div className='NameOfFreeLancerAndNickName'>
-                <h3>majd mahmoud</h3>
-                <h6>Full Stack Developer</h6>
+    <Card
+      className="FindFreeLancerCard"
+      onClick={() => {
+        navigate("/FreeLanceInfo");
+      }}
+    >
+      <CardContent className="CardContent">
+        <div className="LeftSide">
+          <div className="NameAndFirstLater">
+            <div className="FirstLaterOfFreeLancer">MA</div>
+            <div className="NameOfFreeLancerAndNickName">
+              <h3>majd mahmoud</h3>
+              <h6>Full Stack Developer</h6>
             </div>
+          </div>
+          <div className="StatusOfFreeLancer">🟢Avialable now</div>
+          <div className="SkillsOfFreeLancer">
+            <div className="SkillItemOfFreeLancer">React</div>
+            <div className="SkillItemOfFreeLancer">Node </div>
+            <div className="SkillItemOfFreeLancer">AWS</div>
+          </div>
+          <div className="CountOfProjectCompleted">24 project completed</div>
         </div>
-        <div className='StatusOfFreeLancer'>🟢Avialable now</div>
-        <div className='SkillsOfFreeLancer'>
-            <div className='SkillItemOfFreeLancer'>React</div>
-            <div className='SkillItemOfFreeLancer'>Node </div>
-            <div className='SkillItemOfFreeLancer'>AWS</div>
+
+        <div className="RightSide">
+          <div className="monyInHour">$45 / hr</div>
+          <div className="RaitingOfFreeLancer">* 4.9 Expert</div>
+          <button className="InviteButton">Invite</button>
         </div>
-        <div className='CountOfProjectCompleted'>24 project completed</div>
-       </div>
-
-       <div className='RightSide'>
-        <div className='monyInHour'>$45 / hr</div>
-        <div className='RaitingOfFreeLancer'>* 4.9 Expert</div>
-        <button  className='InviteButton'>Invite</button>
-       </div>
-
       </CardContent>
-     
     </Card>
   );
 }
