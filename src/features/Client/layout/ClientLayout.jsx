4d06e9ff7fe0540f848cbfc8c512252faad291d2
Rand <hrand801@gmail.com>
2026-08-04 21:59:19 +0100

    add FreeLink

--- src/features/Client/layout/ClientLayout.jsx
+++ src/features/Client/layout/ClientLayout.jsx
@@ -1,35 +1,3 @@
-// import React, { useState } from 'react';
-// import { Outlet } from 'react-router-dom';
-// import ClientSidebar from '../components/ClientSidebar';
-// import ClientHeader from '../components/ClientHeader';
-// import '../styles/ClientLayout.css';
-
-// export default function ClientLayout() {
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-//   return (
-    
-//     <div className="dashboard-layout">
-
-//     <ClientSidebar
-//         sidebarOpen={sidebarOpen}
-//         setSidebarOpen={setSidebarOpen}
-//     />
-
-//     <div className="main-wrapper">
-
-//         <ClientHeader
-//             setSidebarOpen={setSidebarOpen}
-//         />
-
-//         <main className="page-content">
-//             <Outlet />
-//         </main>
-
-//     </div>
-
-// </div>
-//   );
-// }
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import ClientSidebar from "../components/ClientSidebar";
